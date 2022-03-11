82ab02409c4d446207d6bde3dc5a520d98a3b6ef
Pavel Jakš <stanio250@gmail.com>
2022-03-11 22:50:59 +0100

    experiments

--- Code/models.py
+++ Code/models.py
@@ -486,6 +486,63 @@ class MnistCnnPatt(nn.Module):
         return out
 
 
+class MnistGanGenerator(nn.Module):
+    """
+    base in Broader broader CNN
+    """
+    def __init__(self):
+        super().__init__()
+        self.fourth_linear = nn.Linear(10, 64)
+        self.fourth_activation = nn.ReLU()
+        self.seventh_linear = nn.Linear(64, 512)
+        self.seventh_activation = nn.ReLU()
+        self.eighth_linear = nn.Linear(512, 28 * 28)
+        self.eighth_activation = nn.Sigmoid()
+
+    def forward(self, t: torch.Tensor):
+        dim = 1
+        for i in range(1, len(t.shape)):
+            dim *= t.shape[i]
+        out = t.reshape(len(t), dim)
+        out = self.fourth_activation(self.fourth_linear(out))
+        out = self.seventh_activation(self.seventh_linear(out))
+        out = self.eighth_activation(self.eighth_linear(out))
+        return out.reshape((len(t), 1, 28, 28))
+
+
+class MnistGanDiscriminator(nn.Module):
+    """
+    base in Broader CNN
+    """
+    def __init__(self):
+        super().__init__()
+        self.first_conv_layer = nn.Conv2d(1, 32, kernel_size=9)
+        self.first_activation = nn.ReLU()
+        self.second_pooling = nn.MaxPool2d(4)
+        self.second_activation = nn.ReLU()
+        self.third_linear = nn.Linear(32 * 5 * 5, 512)
+        self.third_activation = nn.ReLU()
+        self.fourth_linear = nn.Linear(512, 512)
+        self.fourth_activation = nn.ReLU()
+        self.fifth_linear = nn.Linear(512, 256)
+        self.fifth_activation = nn.ReLU()
+        self.sixth_linear = nn.Linear(256, 1)
+        self.sixth_activation = nn.Tanh()
+    
+    def forward(self, t: torch.Tensor):
+        out = self.first_activation(self.first_conv_layer(t))
+        out = self.second_activation(self.second_pooling(out))
+        dim = 1
+        for i in range(1, len(out.shape)):
+            dim *= out.shape[i]
+        out = out.reshape(len(t), dim)
+        out = self.third_activation(self.third_linear(out))
+        out = self.fourth_activation(self.fourth_linear(out))
+        out = self.fifth_activation(self.fifth_linear(out))
+        out = self.sixth_activation(1 * self.sixth_linear(out))
+        return out
+
+
 class ModelType(Enum):
     MnistCnnAlfred = MnistCnnAlfred
     MnistCnnBerta = MnistCnnBerta
@@ -503,6 +560,8 @@ class ModelType(Enum):
     MnistMLPNevil = MnistMLPNevil
     MnistCnnOto = MnistCnnOto
     MnistCnnPatt = MnistCnnPatt
+    MnistGanGenerator = MnistGanGenerator
+    MnistGanDiscriminator = MnistGanDiscriminator
 
 
 class ModelManager:
